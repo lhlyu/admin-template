eb763393cfc3bffc40aad3831e847822dfb993ad
lhlyu <misakafs@gmail.com>
2022-04-08 20:11:51 +0800

    移除移动端hover样式

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     plugins: [
+        require('postcss-hover-media-feature')({
+            fallback: true
+        }),
         // 配置 Autoprefixer 插件
         require('autoprefixer')({
             // 游览器最近的两个版本
